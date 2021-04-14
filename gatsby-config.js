a8f7369040c6ae47af6417a630f3905fde8047cd
Stephan Ringel <ringel@adobe.com>
2021-04-14 14:06:23 +0200

    Update deploy workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,5 @@ module.exports = {
     ]
   },
   plugins: [`@adobe/gatsby-theme-aio`],
-  pathPrefix: process.env.PATH_PREFIX || '/camera-raw'
+  pathPrefix: process.env.PATH_PREFIX || '/camera-raw/'
 };
